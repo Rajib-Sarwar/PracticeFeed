30a0be896fc9392b184bc5732bcc9b1ae9e3ac80
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-05 20:30:18 -0500

    added intial project to practice system design to manage a Feed List view

---
+++ PracticeFeed/PracticeFeed.h
@@ -0,0 +1,18 @@
+//
+//  PracticeFeed.h
+//  PracticeFeed
+//
+//  Created by Chowdhury Md Rajib Sarwar on 11/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PracticeFeed.
+FOUNDATION_EXPORT double PracticeFeedVersionNumber;
+
+//! Project version string for PracticeFeed.
+FOUNDATION_EXPORT const unsigned char PracticeFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PracticeFeed/PublicHeader.h>
+
+
