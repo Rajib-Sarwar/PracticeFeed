9bff0d00ed5da507c547c1d9fc8bc487373fce5a
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-05 20:33:02 -0500

    create the FeedItem model

---
+++ PracticeFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  PracticeFeed
+//
+//  Created by Chowdhury Md Rajib Sarwar on 11/5/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
