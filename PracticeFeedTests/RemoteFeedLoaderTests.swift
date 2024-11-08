04dbaa8973299990570f3d1467ba79377e5addd9
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-07 20:02:55 -0500

    delivers invalid data error on non 200 http responses

--- PracticeFeedTests/RemoteFeedLoaderTests.swift
+++ PracticeFeedTests/RemoteFeedLoaderTests.swift
@@ -45,6 +45,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
         
         XCTAssertEqual(captureErrors, [.connectivity])
     }
+    
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (client, sut) = makeSUT()
+        
+        var captureErrors = [RemoteFeedLoader.Error]()
+        sut.load { captureErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.complete(withStatusCode: 400)
+        
+        XCTAssertEqual(captureErrors, [.invalidData])
+    }
 
     // MARK: Helpers
     
@@ -55,18 +67,23 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (Error) -> Void)]()
+        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
         
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)
+            messages[index].completion(nil, response)
         }
     }
 }
