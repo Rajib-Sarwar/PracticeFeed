54366cb342166951b45916baf695ea26c03199c6
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-09 08:46:52 -0500

    replaced hard coded magic number into  variable giving extra context

--- PracticeFeed/Feed API/RemoteFeedLoader.swift
+++ PracticeFeed/Feed API/RemoteFeedLoader.swift
@@ -53,8 +53,9 @@ public final class RemoteFeedLoader {
 }
 
 private class FeedItemMapper {
+    static var OK_200: Int { return 200 }
     static func map(_ response: HTTPURLResponse, _ data: Data) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         
