1bba295fe125490c068820e95af27979903115b3
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-12 15:17:56 -0500

    simply type declaration with a helper method

--- PracticeFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ PracticeFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -37,7 +37,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (client, sut) = makeSUT()
 
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
+        expect(sut, toCompleteWith: failure(.connectivity), when: {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         })
@@ -49,7 +49,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
                 let json = makeItemJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             })
@@ -59,7 +59,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseOnInvalidJSON() {
         let (client, sut) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+        expect(sut, toCompleteWith: failure(.invalidData), when: {
             let ivalidJSON = Data("invalid data".utf8)
             client.complete(withStatusCode: 200, data: ivalidJSON)
         })
@@ -143,6 +143,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
         
         return (feedItem, json)
+    }
+    
+    private func failure(_ error:RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
