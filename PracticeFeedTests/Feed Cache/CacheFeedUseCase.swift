85fbe5945f42fe8355341692e9af9191c9a1f279
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-14 20:29:48 -0500

    Extract system under test into a factory method to protect tests against breaking changes

---
+++ PracticeFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -0,0 +1,62 @@
+//
+//  CacheFeedUseCase.swift
+//  PracticeFeedTests
+//
+//  Created by Chowdhury Md Rajib Sarwar on 11/14/24.
+//
+
+import XCTest
+import PracticeFeed
+
+class LocalFeedLoader {
+    let store: FeedStore
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    public func save(_ items: [FeedItem]) {
+        store.deleteCahedFeed()
+    }
+}
+
+class FeedStore {
+    var deleteCacheFeedCallCount = 0
+    
+    func deleteCahedFeed() {
+        deleteCacheFeedCallCount += 1
+    }
+}
+
+class CacheFeedUseCase: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        return (sut, store)
+    }
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: nil, location: nil, imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+    
+}
