b6a0c113687077cfaa9c1401b30cd9d66e50a190
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-16 18:00:15 -0500

    Moved `LocalFeedItem` and `RemoteFeedItem` in its own file

---
+++ PracticeFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteFeedItem.swift
+//  PracticeFeed
+//
+//  Created by Chowdhury Md Rajib Sarwar on 11/16/24.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
