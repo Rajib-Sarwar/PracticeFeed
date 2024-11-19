f410b36b2e572b00bb25f89fedd2de5e4c7c70e6
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-18 21:43:46 -0500

    validate cache command does not delete less than seven days old cache

--- PracticeFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ PracticeFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -83,7 +83,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receiveMessages, [.retrieve])
     }
     
-    func test_load_HasNosideEffectsOnEmptyCache() {
+    func test_load_hasNosideEffectsOnEmptyCache() {
         let (sut, store) = makeSUT()
         
         sut.load { _ in }
@@ -92,7 +92,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receiveMessages, [.retrieve])
     }
     
-    func test_load_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+    func test_load_hasNoSideEffectOnLessThanSevenDaysOldCache() {
         let feed = uniqueImageFeed()
         let fixedCurrentDate = Date()
         let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
