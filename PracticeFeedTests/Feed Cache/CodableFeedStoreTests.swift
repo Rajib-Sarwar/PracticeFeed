a7cbbc0130450a36545944507ac40ace44f1ca97
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-20 20:09:47 -0500

    retrieving from empty cache delivering empty cache

---
+++ PracticeFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodableFeedStoreTests.swift
+//  PracticeFeedTests
+//
+//  Created by Chowdhury Md Rajib Sarwar on 11/20/24.
+//
+
+import XCTest
+import PracticeFeed
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+class CodableFeedStoreTests: XCTestCase {
+    
+    func test_retrieve_deliverEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
