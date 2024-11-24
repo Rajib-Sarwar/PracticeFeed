db6970218211515c0c05dd52ae375a96e0f05fd0
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-11-23 21:45:02 -0500

    create FeedStoreSpecs that must be implemented by any FeedStore test implementtaion to gurantee it meets spec

--- PracticeFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ PracticeFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,34 +8,8 @@
 import XCTest
 import PracticeFeed
 
-protocol FeedStoreSpecs {
-    func test_retrieve_deliverEmptyOnEmptyCache()
-    func test_retrieve_hasNoSideEffectOnEmptyCache()
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectOnNonEmptyCache()
-    
-    func test_insert_overridesPreviouslyInsertedCacheValues()
-    
-    func test_delete_hasNoSideEffectsOnEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()
-    
-    func test_storeSideEffects_runSerially()
-}
-
-protocol FailableRetrieveFeedStoreSpecs {
-    func test_retrieve_deliversFailureOnRetrievalError()
-    func test_retrieve_hasNoSideEffectsOnFailure()
-}
-protocol FailableInsertFeedStoreSpecs {
-    func test_insert_deliversErrorOnInsertionError()
-    func test_insert_hasNoSideEffectsOnInsertionError()
-}
-protocol FailableDeleteFeedStoreSpecs {
-    func test_delete_deliversErrorOnDeletionCache()
-    func test_delete_hasNoSideEffectOnDeletionError()
-}
-
-class CodableFeedStoreTests: XCTestCase {
+typealias FailableFeedStore = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
+class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
    
     override func setUp() {
         super.setUp()
