e2da4dd4110cd34e2f4a4c73960f9845a320081b
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-12-06 14:51:28 -0500

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` delivers no items on empty cache

--- PracticeFeedCacheIntegrationTests/PracticeFeedCacheIntegrationTests.swift
+++ PracticeFeedCacheIntegrationTests/PracticeFeedCacheIntegrationTests.swift
@@ -6,9 +6,47 @@
 //
 
 import XCTest
+import PracticeFeed
 
 final class PracticeFeedCacheIntegrationTests: XCTestCase {
 
-   
+    func test_load_deliversNoItemOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for cache retrieval")
+        sut.load { result in
+            switch result {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, [], "Expected empty feed")
+                 
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
 
+    // MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = testSpecificStoreURL
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private var testSpecificStoreURL: URL {
+        return FileManager.default.urls(for: .cachesDirectory,
+                                        in: .userDomainMask).first!.appendingPathComponent("\(type(of: self )).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory,
+                                        in: .userDomainMask).first!
+    }
 }
