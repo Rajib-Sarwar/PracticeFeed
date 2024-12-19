b72c76122f1bbb234ea40d9ef62040c813c33097
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2024-12-18 22:24:04 -0500

    Remove PracticeFeed module import from FeedViewController file since it does not depend on any PracticeFeed component.

--- PracticeFeed/PracticeFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ PracticeFeed/PracticeFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import PracticeFeed
 
 final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     private var refreshController: FeedRefreshViewController?
